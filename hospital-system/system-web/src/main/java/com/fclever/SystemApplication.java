29d69645a351b03d1a24abcc0d15d9c205807bb3
fclecer <1393906015@qq.com>
2020-10-16 13:10:53 +0800

    系统模块下控制层模块system-web创建并配置

---
+++ hospital-system/system-web/src/main/java/com/fclever/SystemApplication.java
@@ -0,0 +1,20 @@
+package com.fclever;
+
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Fclever
+ * @create 2020-10-16 08:43
+ */
+@SpringBootApplication // SpringBoot项目核心注解，开启自动配置
+@MapperScan(basePackages = {"com.fclever.mapper"})  // 指定接口所在包，对应包下的接口编译后会生成相应实现类
+@EnableDubbo // 启动Dubbo
+public class SystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SystemApplication.class, args);
+        System.out.println("系统模块hospital-system启动成功");
+    }
+}
