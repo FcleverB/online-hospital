1c367b7343d74b513d7587a10dee5cecb6a1f038
fclecer <1393906015@qq.com>
2020-10-17 07:59:54 +0800

    进销存模块下业务逻辑模块erp-service创建并配置

---
+++ hospital-erp/erp-service/src/main/java/com/fclever/ErpApplication.java
@@ -0,0 +1,19 @@
+package com.fclever;
+
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Fclever
+ * @create 2020-10-17 07:35
+ */
+@SpringBootApplication
+@MapperScan(basePackages = {"com.fclever.mapper"})
+@EnableDubbo
+public class ErpApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ErpApplication.class, args);
+    }
+}
