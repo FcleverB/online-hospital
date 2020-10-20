86ba0db1a2728e749da8dcc582c4edb00f1c4808
fclecer <1393906015@qq.com>
2020-10-20 09:10:14 +0800

    系统管理控制层模块system-web添加mybatis-plus插件配置

---
+++ hospital-system/system-web/src/main/java/com/fclever/config/mybatisplus/MybatisPlusConfig.java
@@ -0,0 +1,30 @@
+package com.fclever.config.mybatisplus;
+
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.pagination.optimize.JsqlParserCountOptimize;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * MybatisPlus分页插件
+ *          官网对应位置：https://baomidou.com/guide/page.html
+ * @author Fclever
+ * @create 2020-10-20 09:00
+ */
+@EnableTransactionManagement
+@Configuration
+public class MybatisPlusConfig {
+
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+        // paginationInterceptor.setOverflow(false);
+        // 设置最大单页限制数量，默认 500 条，-1 不受限制
+        // paginationInterceptor.setLimit(500);
+        // 开启 count 的 join 优化,只针对部分 left join
+        paginationInterceptor.setCountSqlParser(new JsqlParserCountOptimize(true));
+        return paginationInterceptor;
+    }
+}
