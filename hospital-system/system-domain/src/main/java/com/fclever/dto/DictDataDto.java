be147b46f39d84de843cc6cf3828154c8e42c3cc
fclecer <1393906015@qq.com>
2020-11-03 21:08:51 +0800

    系统管理hospital-system中DictDataDto数据传输类编写

--- hospital-system/system-domain/src/main/java/com/fclever/dto/DictDataDto.java
+++ hospital-system/system-domain/src/main/java/com/fclever/dto/DictDataDto.java
@@ -1,8 +1,5 @@
 package com.fclever.dto;
 
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableField;
-import com.baomidou.mybatisplus.annotation.TableId;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
@@ -14,45 +11,63 @@ import javax.validation.constraints.NotNull;
 import java.util.Date;
 
 /**
- * 字典类型数据传输类
- * 数据传输类DataTransferObject
- *      查询和添加时不一定操作全部的
+ * 字典数据数据传输类
  * @author Fclever
- * @create 2020-11-02 20:21
+ * @create 2020-11-03 20:35
  */
-@ApiModel(value="com-fclever-dto-DictTypeDto")
+@ApiModel(value="com-fclever-domain-DictData")
 @Data
+@EqualsAndHashCode(callSuper=true)
 @AllArgsConstructor
 @NoArgsConstructor
-@EqualsAndHashCode(callSuper = false)
-public class DictTypeDto extends BaseDto{
+public class DictDataDto extends BaseDto{
 
     /**
-     * 字典主键
+     * 字典编码
      */
-    @ApiModelProperty(value="字典主键")
-    private Long dictId;
+    @ApiModelProperty(value="字典编码")
+    private Long dictCode;
 
     /**
-     * 字典名称
+     * 字典排序
      */
-    @NotNull(message = "字典名称不能为空")
-    @ApiModelProperty(value="字典名称")
-    private String dictName;
+    @ApiModelProperty(value="字典排序")
+    @NotNull(message = "字典排序不能为空")
+    private Integer dictSort;
+
+    /**
+     * 字典标签
+     */
+    @ApiModelProperty(value="字典标签")
+    @NotNull(message = "字典标签不能为空")
+    private String dictLabel;
+
+    /**
+     * 字典键值
+     */
+    @ApiModelProperty(value="字典键值")
+    @NotNull(message = "字典键值不能为空")
+    private String dictValue;
 
     /**
      * 字典类型
      */
-    @NotNull(message = "字典类型不能为空")
     @ApiModelProperty(value="字典类型")
+    @NotNull(message = "字典类型不能为空")
     private String dictType;
 
     /**
      * 状态（0正常 1停用）
      */
-    @NotNull(message = "状态不能为空")
     @ApiModelProperty(value="状态（0正常 1停用）")
+    @NotNull(message = "状态不能为空")
     private String status;
+
+    /**
+     * 备注
+     */
+    @ApiModelProperty(value="备注")
+    private String remark;
 
     /**
      * 创建者
@@ -77,10 +92,4 @@ public class DictTypeDto extends BaseDto{
      */
     @ApiModelProperty(value="更新时间")
     private Date updateTime;
-
-    /**
-     * 备注
-     */
-    @ApiModelProperty(value="备注")
-    private String remark;
 }
