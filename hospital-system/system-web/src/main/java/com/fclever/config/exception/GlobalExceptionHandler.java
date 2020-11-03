f0ba90217e4ba6880eb04ce75c55481c4bcdf8e0
fclecer <1393906015@qq.com>
2020-11-03 19:04:49 +0800

    系统管理hospital-system中添加字典类型时不含参数保存的异常信息格式调整

--- hospital-system/system-web/src/main/java/com/fclever/config/exception/GlobalExceptionHandler.java
+++ hospital-system/system-web/src/main/java/com/fclever/config/exception/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.fclever.config.exception;
 
 import com.fclever.vo.AjaxResult;
+import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -22,22 +24,42 @@ import java.util.Map;
 public class GlobalExceptionHandler {
 
     /**
-     * 方法参数无效异常
+     * 方法参数无效异常---如果页面传参是json对象，当json对象为空时能触发
      * 当系统出现MethodArgumentNotValidException这个异常时，会调用下面的方法
      * @param e
      * @return
      */
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
     public AjaxResult jsonErrorHandler(MethodArgumentNotValidException e){
-        List<Map<String,Object>> list = new ArrayList<>();
-        List<ObjectError> allErrors = e.getBindingResult().getAllErrors();
+        return getAjaxResult(e.getBindingResult());
+    }
+
+    /**
+     *
+     * 当系统出现BindException这个异常时，会调用下面的方法
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(value = BindException.class)
+    public AjaxResult jsonErrorHandler(BindException e){
+        return getAjaxResult(e.getBindingResult());
+    }
+
+    /**
+     * 抽取公共模块
+     * @param bindingResult
+     * @return
+     */
+    private AjaxResult getAjaxResult(BindingResult bindingResult) {
+        List<Map<String, Object>> list = new ArrayList<>();
+        List<ObjectError> allErrors = bindingResult.getAllErrors();
         for (ObjectError allError : allErrors) {
             Map<String, Object> map = new HashMap<>();
             map.put("defaultMessage", allError.getDefaultMessage());
             map.put("objectName", allError.getObjectName());
             // 转换类型，获取属性 FieldError extends ObjectError
             // field属性需要从FieldError获取
-            FieldError fieldError = (FieldError)allError;
+            FieldError fieldError = (FieldError) allError;
             map.put("field", fieldError.getField());
             list.add(map);
         }
