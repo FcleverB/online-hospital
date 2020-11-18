24da444f3068e3644362f9e92421033baac99b8e
fclecer <1393906015@qq.com>
2020-11-18 15:01:12 +0800

    系统管理hospital-system修改操作类型码表sys_oper_type对应枚举修改

--- hospital-commons/src/main/java/com/fclever/aspectj/enums/BusinessType.java
+++ hospital-commons/src/main/java/com/fclever/aspectj/enums/BusinessType.java
@@ -3,6 +3,8 @@ package com.fclever.aspectj.enums;
 /**
  * 业务类型枚举类
  *      增删改和其他
+ *      这些类型数据必须和数据库的对应码表的dict_value值一致
+ *      ordinal序数从0开始
  * @author Fclever
  * @create 2020-11-16 12:28
  */
@@ -31,35 +33,5 @@ public enum BusinessType {
     /**
      * 同步字典数据到缓存
      */
-    CACHEASYNC,
-
-    /**
-     * 授权
-     */
-    GRANT,
-
-    /**
-     * 导出
-     */
-    EXPORT,
-
-    /**
-     * 导入
-     */
-    IMPORT,
-
-    /**
-     * 强退
-     */
-    FORCE,
-
-    /**
-     * 生成代码
-     */
-    GENCODE,
-
-    /**
-     * 清空数据
-     */
-    CLEAN
+    CACHEASYNC
 }
