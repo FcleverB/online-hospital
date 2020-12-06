2ef6318cc385baf3cb8f302525fc58b1b22234a0
fclecer <1393906015@qq.com>
2020-12-06 12:20:30 +0800

    系统管理hospital-system中检查项目实体校验添加

--- hospital-system/system-domain/src/main/java/com/fclever/dto/CheckItemDto.java
+++ hospital-system/system-domain/src/main/java/com/fclever/dto/CheckItemDto.java
@@ -12,6 +12,8 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.util.Date;
 
@@ -39,41 +41,48 @@ public class CheckItemDto extends BaseDto {
      * 项目名称
      */
     @ApiModelProperty(value="项目名称")
+    @NotBlank(message = "项目名称不能为空")
     private String checkItemName;
 
     /**
      * 关键字【查询用】
      */
     @ApiModelProperty(value="关键字【查询用】")
+    @NotBlank(message = "关键字不能为空")
     private String keywords;
 
     /**
      * 项目单价
      */
     @ApiModelProperty(value="项目单价")
+    @NotNull(message = "项目单价不能为空")
     private BigDecimal unitPrice;
 
     /**
      * 项目成本
      */
     @ApiModelProperty(value="项目成本")
+    @NotBlank(message = "项目成本不能为空")
     private BigDecimal cost;
 
     /**
      * 计量单位
      */
     @ApiModelProperty(value="计量单位")
+    @NotBlank(message = "计量单位不能为空")
     private String unit;
 
     /**
      * 项目类别IDsxt_sys_dict_type
      */
     @ApiModelProperty(value="项目类别IDsxt_sys_dict_type")
+    @NotBlank(message = "项目类别不能为空")
     private String typeId;
 
     /**
      * 状态0正常1停用 sxt_sys_dict_type
      */
     @ApiModelProperty(value="状态0正常1停用 sxt_sys_dict_type")
+    @NotBlank(message = "状态不能为空")
     private String status;
 }
