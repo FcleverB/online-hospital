dfd59e94bee0ce99347ed833ab64db8c1e21a2d0
fclecer <1393906015@qq.com>
2020-12-20 15:52:07 +0800

    进销存管理hospital-erp中采购入库中重新生成InventoryLog入库操作信息表代码

--- hospital-erp/erp-domain/src/main/java/com/fclever/domain/InventoryLog.java
+++ hospital-erp/erp-domain/src/main/java/com/fclever/domain/InventoryLog.java
@@ -15,14 +15,15 @@ import lombok.NoArgsConstructor;
 
 /**
 @author Fclever
-@create 2020-12-20 15:07
+@create 2020-12-20 15:50
 */
+
 /**
-    * 入库操作信息记录表
-    */
-@ApiModel(value="com-fclever-domain-InventoryLog")
+ * 入库操作信息记录表
+ */
+@ApiModel(value = "com-fclever-domain-InventoryLog")
 @Data
-@EqualsAndHashCode(callSuper=true)
+@EqualsAndHashCode(callSuper = true)
 @AllArgsConstructor
 @NoArgsConstructor
 @TableName(value = "stock_inventory_log")
@@ -31,126 +32,105 @@ public class InventoryLog extends BaseEntity {
      * 入库ID
      */
     @TableId(value = "inventory_log_id", type = IdType.INPUT)
-    @ApiModelProperty(value="入库ID")
+    @ApiModelProperty(value = "入库ID")
     private String inventoryLogId;
 
     /**
      * 入库单据ID
      */
     @TableField(value = "purchase_id")
-    @ApiModelProperty(value="入库单据ID")
+    @ApiModelProperty(value = "入库单据ID")
     private String purchaseId;
 
     /**
      * 药品ID
      */
     @TableField(value = "medicines_id")
-    @ApiModelProperty(value="药品ID")
+    @ApiModelProperty(value = "药品ID")
     private String medicinesId;
 
     /**
      * 入库数量
      */
     @TableField(value = "inventory_log_num")
-    @ApiModelProperty(value="入库数量")
+    @ApiModelProperty(value = "入库数量")
     private Integer inventoryLogNum;
 
     /**
      * 批发价
      */
     @TableField(value = "trade_price")
-    @ApiModelProperty(value="批发价")
+    @ApiModelProperty(value = "批发价")
     private BigDecimal tradePrice;
-
-    /**
-     * 处方价
-     */
-    @TableField(value = "prescription_price")
-    @ApiModelProperty(value="处方价")
-    private BigDecimal prescriptionPrice;
 
     /**
      * 批发额
      */
     @TableField(value = "trade_total_amount")
-    @ApiModelProperty(value="批发额")
+    @ApiModelProperty(value = "批发额")
     private BigDecimal tradeTotalAmount;
-
-    /**
-     * 处方额
-     */
-    @TableField(value = "prescription_total_amount")
-    @ApiModelProperty(value="处方额")
-    private BigDecimal prescriptionTotalAmount;
 
     /**
      * 药品生产批次号
      */
     @TableField(value = "batch_number")
-    @ApiModelProperty(value="药品生产批次号")
+    @ApiModelProperty(value = "药品生产批次号")
     private String batchNumber;
 
     /**
      * 药品名称
      */
     @TableField(value = "medicines_name")
-    @ApiModelProperty(value="药品名称")
+    @ApiModelProperty(value = "药品名称")
     private String medicinesName;
 
     /**
      * 药品分类 sys_dict_data表his_medicines_type
      */
     @TableField(value = "medicines_type")
-    @ApiModelProperty(value="药品分类 sys_dict_data表his_medicines_type")
+    @ApiModelProperty(value = "药品分类 sys_dict_data表his_medicines_type")
     private String medicinesType;
 
     /**
      * 处方类型 sys_dict_data表his_prescription_type
      */
     @TableField(value = "prescription_type")
-    @ApiModelProperty(value="处方类型 sys_dict_data表his_prescription_type")
+    @ApiModelProperty(value = "处方类型 sys_dict_data表his_prescription_type")
     private String prescriptionType;
 
     /**
      * 生产厂家ID
      */
     @TableField(value = "producter_id")
-    @ApiModelProperty(value="生产厂家ID")
+    @ApiModelProperty(value = "生产厂家ID")
     private String producterId;
 
     /**
      * 换算量
      */
     @TableField(value = "conversion")
-    @ApiModelProperty(value="换算量")
+    @ApiModelProperty(value = "换算量")
     private Integer conversion;
 
     /**
      * 单位（g/条）
      */
     @TableField(value = "unit")
-    @ApiModelProperty(value="单位（g/条）")
+    @ApiModelProperty(value = "单位（g/条）")
     private String unit;
-
-    /**
-     * 供应商ID
-     */
-    @TableField(value = "provider_id")
-    @ApiModelProperty(value="供应商ID")
-    private String providerId;
 
     /**
      * 创建时间
      */
     @TableField(value = "create_time")
-    @ApiModelProperty(value="创建时间")
+    @ApiModelProperty(value = "创建时间")
     private Date createTime;
 
     /**
      * 创建者
      */
     @TableField(value = "create_by")
-    @ApiModelProperty(value="创建者")
+    @ApiModelProperty(value = "创建者")
     private String createBy;
 
     public static final String COL_INVENTORY_LOG_ID = "inventory_log_id";
@@ -163,11 +143,7 @@ public class InventoryLog extends BaseEntity {
 
     public static final String COL_TRADE_PRICE = "trade_price";
 
-    public static final String COL_PRESCRIPTION_PRICE = "prescription_price";
-
     public static final String COL_TRADE_TOTAL_AMOUNT = "trade_total_amount";
-
-    public static final String COL_PRESCRIPTION_TOTAL_AMOUNT = "prescription_total_amount";
 
     public static final String COL_BATCH_NUMBER = "batch_number";
 
@@ -182,8 +158,6 @@ public class InventoryLog extends BaseEntity {
     public static final String COL_CONVERSION = "conversion";
 
     public static final String COL_UNIT = "unit";
-
-    public static final String COL_PROVIDER_ID = "provider_id";
 
     public static final String COL_CREATE_TIME = "create_time";
 
--- hospital-erp/erp-mapper/src/main/java/com/fclever/mapper/InventoryLogMapper.java
+++ hospital-erp/erp-mapper/src/main/java/com/fclever/mapper/InventoryLogMapper.java
@@ -4,8 +4,8 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.fclever.domain.InventoryLog;
 
 /**
-@author Fclever
-@create 2020-12-20 15:07
-*/
+ * @author Fclever
+ * @create 2020-12-20 15:50
+ */
 public interface InventoryLogMapper extends BaseMapper<InventoryLog> {
 }
