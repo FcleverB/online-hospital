a13ebea83308b45d81040cb98bbda5f269678944
fclecer <1393906015@qq.com>
2020-12-20 15:08:57 +0800

    进销存管理hospital-erp中采购入库中生产InventoryLog入库操作信息表代码

---
+++ hospital-erp/erp-mapper/src/main/java/com/fclever/mapper/InventoryLogMapper.java
@@ -0,0 +1,11 @@
+package com.fclever.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.fclever.domain.InventoryLog;
+
+/**
+@author Fclever
+@create 2020-12-20 15:07
+*/
+public interface InventoryLogMapper extends BaseMapper<InventoryLog> {
+}
