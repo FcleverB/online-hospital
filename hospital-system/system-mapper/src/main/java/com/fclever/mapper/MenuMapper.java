60d5459657f42206d281914f74dfe1d4f30323b0
fclecer <1393906015@qq.com>
2021-02-17 23:02:01 +0800

    online-hospital中权限管理实现（不同用户-不同角色-显示不同菜单）

--- hospital-system/system-mapper/src/main/java/com/fclever/mapper/MenuMapper.java
+++ hospital-system/system-mapper/src/main/java/com/fclever/mapper/MenuMapper.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.fclever.domain.Menu;
 import org.apache.ibatis.annotations.Param;
 
+import java.io.Serializable;
 import java.util.List;
 
 /**
@@ -14,7 +15,8 @@ public interface MenuMapper extends BaseMapper<Menu> {
 
     /**
      * 根据菜单id查询它的子菜单的个数
-     *      按alt+enter  可以辅助生成xml中SQL，并且为形参添加注解
+     * 按alt+enter  可以辅助生成xml中SQL，并且为形参添加注解
+     *
      * @param menuId
      * @return 查询到的子菜单个数
      */
@@ -22,8 +24,17 @@ public interface MenuMapper extends BaseMapper<Menu> {
 
     /**
      * 根据角色id查询该角色已经分配的所有菜单id（仅仅查询子菜单id）
-     * @param roleId    角色id
-     * @return  查询结果
+     *
+     * @param roleId 角色id
+     * @return 查询结果
      */
     List<Long> queryMenuIdsByRoleId(@Param("roleId") Long roleId);
+
+    /**
+     * 根据用户Id查询用户所拥有的菜单
+     *
+     * @param userId 用户Id
+     * @return 菜单结果
+     */
+    List<Menu> selectMenuListByUserId(@Param("userId") Serializable userId);
 }
